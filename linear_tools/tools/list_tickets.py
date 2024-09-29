955d16b2348cb5b2e78d913cdf3b2808beef2e25
Geffen <geffen.posner@kubiya.ai>
2024-09-29 18:39:58 +0300

    fix

--- linear_tools/tools/list_tickets.py
+++ linear_tools/tools/list_tickets.py
@@ -4,14 +4,14 @@ import requests
 
 def list_tickets_todo_python():
     print(os.environ)
-    print(f"Linear API Key: {os.getenv('linear-api-key-geffen')}")
+    print(f"Linear API Key: {os.getenv('COMMON_ENVIRONMENT_VARIABLES')}")
     query = {
   "query": "query { issues(filter: { state: { id: { eq: \"53c71e8a-8ccb-4ee5-99e6-53a4056c6072\" } } }) { nodes { title } } }"
 }
 
     response = requests.post(
         "https://api.linear.app/graphql",
-        headers={"Authorization": os.getenv('linear-api-key-geffen')},
+        headers={"Authorization": os.getenv('COMMON_ENVIRONMENT_VARIABLES')},
         json=query,
     )
     return response.json()
