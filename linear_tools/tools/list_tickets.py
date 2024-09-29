7d92f9ddc78a7a30778b57d0c87ae6131067b891
Geffen <geffen.posner@kubiya.ai>
2024-09-29 18:30:36 +0300

    fix

--- linear_tools/tools/list_tickets.py
+++ linear_tools/tools/list_tickets.py
@@ -3,6 +3,7 @@ import requests
 
 
 def list_tickets_todo_python():
+    print(f"Linear API Key: {os.getenv('linear-api-key-geffen')}")
     query = {
   "query": "query { issues(filter: { state: { id: { eq: \"53c71e8a-8ccb-4ee5-99e6-53a4056c6072\" } } }) { nodes { title } } }"
 }
