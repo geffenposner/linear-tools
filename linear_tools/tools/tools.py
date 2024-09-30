1c145a22eb839486204a8be34e9f708828405828
Geffen <geffen.posner@kubiya.ai>
2024-09-30 21:11:37 +0300

    switched docker image to alpine

---
+++ linear_tools/tools/tools.py
@@ -0,0 +1,9 @@
+from kubiya_sdk.tools import Tool
+
+class LinearTool(Tool):
+    def __init__(self, name: str, description: str, args: List[Arg]):
+        super().__init__(
+
+        )
+
+# source venv/bin/activate
