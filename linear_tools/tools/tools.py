e3790762ba19f41c1ee6621c78b0c0f94cc12b49
Geffen <geffen.posner@kubiya.ai>
2024-10-13 13:36:26 +0300

    removed tools.py content to test

--- linear_tools/tools/tools.py
+++
@@ -1,9 +0,0 @@
-from kubiya_sdk.tools import Tool
-
-class LinearTool(Tool):
-    def __init__(self, name: str, description: str, args: List[Arg]):
-        super().__init__(
-
-        )
-
-# source venv/bin/activate
