276da1543d1b2a663285cd5e2030af41bc6434cf
Geffen <geffen.posner@kubiya.ai>
2024-10-20 14:17:25 +0300

    omer 4

--- linear_tools/tools/common.py
+++ linear_tools/tools/common.py
@@ -2,7 +2,7 @@ from kubiya_sdk.tools import FileSpec
 
 # Common environment variables for Linear
 COMMON_ENVIRONMENT_VARIABLES = [
-    "COMMON_ENVIRONMENT_VARIABLES",
+    #"COMMON_ENVIRONMENT_VARIABLES",
 ]
 
 # Common secrets for Linear
