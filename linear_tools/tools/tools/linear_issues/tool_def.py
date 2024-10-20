e704f26736b42e563296b4db752a0781ddf49da0
Geffen <geffen.posner@kubiya.ai>
2024-10-20 15:51:19 +0300

    transfered to kubiya bundle

---
+++ linear_tools/tools/tools/linear_issues/tool_def.py
@@ -0,0 +1,47 @@
+from . import list_tickets_dynamic
+
+import inspect
+
+
+from kubiya_sdk.tools.models import Tool, Arg, FileSpec
+from kubiya_sdk.tools.registry import tool_registry
+
+from .common import COMMON_ENVIRONMENT_VARIABLES, COMMON_SECRET_VARIABLES
+
+LINEAR_ICON_URL = "https://logowik.com/content/uploads/images/linear-app8372.logowik.com.webp"
+
+list_tickets_by_workflow_state_id = Tool(
+    name="list_tickets_by_workflow_state_id",
+    icon_url=LINEAR_ICON_URL,
+    type="docker",
+    image="python:3.12",
+    description="lists Linear tickets with workflow state ID {stateID}",
+    args=[Arg(name="stateID", description="workflow stateID of the tickets you want to see", required=True)],
+    content="""
+curl -LsSf https://astral.sh/uv/install.sh | sh > /dev/null 2>&1
+. $HOME/.cargo/env
+
+uv venv > /dev/null 2>&1
+. .venv/bin/activate > /dev/null 2>&1
+
+uv pip install -r /tmp/requirements.txt > /dev/null 2>&1
+
+python /tmp/list_tickets_dynamic.py "{{ .stateID }}"
+""",
+    env=COMMON_ENVIRONMENT_VARIABLES,
+    secrets=COMMON_SECRET_VARIABLES,
+    with_files=[
+        FileSpec(
+            destination="/tmp/list_tickets_dynamic.py",
+            content=inspect.getsource(list_tickets_dynamic),
+        ),
+        FileSpec(
+            destination="/tmp/requirements.txt",
+            content="",  # Add any requirements here
+        ),
+    ],
+)
+
+
+tool_registry.register("linear", list_tickets_by_workflow_state_id)
+
